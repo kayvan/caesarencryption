62e7b9ad192382a8e950c6717c106a4c7d85f7cf
kayvan <k.ebadzadeh@gmail.com>
2020-03-04 20:27:04 +0100

    Initial commit

---
+++ src/juliuscaesar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class juliuscaesar {
+    public static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Please enter your word: ");
+        String in = sc.next() , enc = "" ;
+        for(int i = 0 ; i < in.length() ; i++) {
+            if( in.charAt(i)== 'z'){
+                enc+=(char) ('a');
+            }
+            else
+                enc += (char) (in.charAt(i)+1);
+
+        }
+        enc = enc.toUpperCase();
+        System.out.println(enc);
+    }
+}
